aac7073be78827539255e431027efa2c049c76f4
Glenn Mesel <37759816+godzillamesel@users.noreply.github.com>
2019-09-17 09:16:29 +0200

    Create install.bash
    Installation tool for global access of the script.

---
+++ install.bash
@@ -0,0 +1,10 @@
+!#/bin/bash
+apt-get install wget tail grep -y
+cd /tmp
+rm oaftl.bash
+rm -rf /usr/bin/oaftl.bash
+rm -rf /usr/bin/oaftl
+wget /tmp/oaftl.bash https://raw.githubusercontent.com/godzillamesel/oaftl.bash/master/oaftl.bash
+mv /tmp/oaftl.bash /usr/bin/oaftl
+chmod +x /usr/bin/oaftl
+echo -e "\n\nIf you got no error codes here then we can say that the installation has finished."
