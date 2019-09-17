f63282ba2111f43d8891e9faeb47891be14a6f00
Glenn Mesel <37759816+godzillamesel@users.noreply.github.com>
2019-09-17 10:07:42 +0200

    Update install.bash

--- install.bash
+++ install.bash
@@ -1,4 +1,5 @@
 !#/bin/bash
+echo -e "Welcome to OAFTL-installation"
 ##ROOT CHECK##
 if [ "$EUID" -ne 0 ]; then
         echo "Please run this as root"
@@ -9,9 +10,13 @@ apt-get install wget tail grep -y #installing dependencies
 rm -rf /tmp/oaftl.bash*    #removing old crap
 rm -rf /usr/bin/oaftl.bash #removing old crap
 rm -rf /usr/bin/oaftl	   #removing old crap
-cd /tmp/ #navigating to the correct folder
-wget https://raw.githubusercontent.com/godzillamesel/oaftl.bash/master/oaftl.bash #downloading the main file
-mv /tmp/oaftl.bash /usr/bin/oaftl | echo "Moved file from /tmp/oaftl.bash to /usr/bin/oaftl without errors" || echo "Error moving the file" #Moving main file to /usr/bin/oaftl
-chmod +x /usr/bin/oaftl #Giving permissions to exec the script
-echo -e "\n\nIf you did not get any errors above here, then we can say that the installation has finished sucsessfully."
-
+cd /tmp/
+echo "Starting download of main file"
+wget https://raw.githubusercontent.com/godzillamesel/oaftl.bash/master/oaftl.bash -q --show-progress  #downloading the main file
+echo "Finished downloading the main file"
+echo "Starting installation:"
+echo "Moving  /tmp/oaftl.bash to /usr/bin/oaftl"
+mv /tmp/oaftl.bash /usr/bin/oaftl | echo "Moved file from /tmp/oaftl.bash to /usr/bin/oaftl without errors" || echo "Error moving the file"
+echo -e "Giving permissions to run script\n\n\n"
+chmod +x /usr/bin/oaftl
+echo -e "\n\nIf you did not get any errors above here, then we can say that the installation has finished sucsessfully.\n\n\n"
