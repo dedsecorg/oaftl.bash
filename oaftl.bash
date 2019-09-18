6adc204c19a5248378fd8278ad3019e08c76d1c8
Glenn Mesel <37759816+godzillamesel@users.noreply.github.com>
2019-09-18 19:58:29 +0200

    Update oaftl.bash

--- oaftl.bash
+++ oaftl.bash
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/bin/env bash
 #Variables for making easy changes.
 version=1.1 #Version number
 description="\n\nThis program searches for a searchword and printes that line in a live file.\n\nIt also can be used as 'tail -f -n +1' without searchword\n"
 
 helpfile="________________________________________________________________________________\n|                                  OAFTL - Helpfile                             |\n|                                                                               |\n|Usage - $0  'directory to file' 'searchword'                         |                                          \n|Show this help menu with $0 -h                                       |\n|                                                                               |\n|                                                                               |\n________________________________________________________________________________"
 #Options:
-while getopts ":hvd:" opt; do
+while getopts ":hvdU:" opt; do
   case ${opt} in
     h ) echo -e "$description"
 	echo -e "$helpfile"
@@ -26,9 +26,9 @@ while getopts ":hvd:" opt; do
    U  ) 
 	read -p "Uninstall solicited, continue (y/n)?" choice
 	case "$choice" in 
-  	y|Y ) echo "yes";;
-  	n|N ) echo "no";;
-  	* ) echo "invalid";;
+  	y|Y|yes ) echo "Ok, uninstalling." && rm -rf $0;;
+  	n|N|no ) echo "Aborted, run the command !! to try again";;
+  	* ) echo "Invalid, try again with y/Y/n/N" && oaftl -U;;
 esac
   esac
 done
